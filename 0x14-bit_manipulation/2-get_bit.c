0fe4fd2e2a12880c3fb2990eb083c03801e4dfab
Esther <estherguravi9@gmail.com>
2023-04-06 06:20:20 -0700

    binary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,21 +12,21 @@ int get_bit(unsigned long int n, unsigned int index)
 
     if (index >= 64)
     {
-        return -1;
+        return (-1);
     }
 
     for (j = 0; j <= 63; j++)
     {
         if (index == j)
         {
-            return n & 1;
+            return (n & 1);
         }
 
         n >>= 1;
 
         if (n == 0 && j < 63)
         {
-            return 0;
+            return (0);
         }
     }
 
