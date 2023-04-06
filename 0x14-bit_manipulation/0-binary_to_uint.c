57e42249db96b3bbd267c175bde90355fe97f696
Esther <estherguravi9@gmail.com>
2023-04-06 02:29:49 -0700

    manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - covert a binary number.
+ * @b: binary number.
+ * Return: converted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int result;
+    int len, base;
+
+    if (!b)
+        return (0);
+
+    result = 0;
+
+    for (len = 0; b[len] != '\0'; len++)
+        ;
+
+    for (len--, base = 1; len >= 0; len--, base *= 2)
+    {
+        if (b[len] != '0' && b[len] != '1')
+        {
+            return (0);
+        }
+
+        if (b[len] & 1)
+        {
+            result += base;
+        }
+    }
+
+    return (result);
+}
