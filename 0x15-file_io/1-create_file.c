0324b9f7cf2278389b9221f75e8c38faac875386
Esther <estherguravi9@gmail.com>
2023-04-07 03:02:05 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: Name of text file created.
+ * @text_content: content of the file created.
+ * Return: On success, 1.
+ * On failure, -1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+    if (!filename)
+        return (-1);
+
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd == -1)
+        return (-1);
+
+    if (text_content) {
+        int len = 0;
+        while (text_content[len] != '\0') {
+            len++;
+        }
+
+        if (write(fd, text_content, len) != len) {
+            close(fd);
+            return (-1);
+        }
+    }
+
+    close(fd);
+    return (1);
+}
