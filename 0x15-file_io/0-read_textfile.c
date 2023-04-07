d030cd2de2a5de3c40d386754ef6c901e05cfaa6
Esther <estherguravi9@gmail.com>
2023-04-07 02:43:51 -0700

    reads a text file and prints it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - eads a text file and prints it to the POSIX std output.
+ * @filename: Textfile name.
+ * @letters: Size.
+ * Return: Actual number of letters it could read and print.
+ * if the file can not be opened or read, return 0.
+ */
+ 
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int file_desc, letters_read = 0;
+    char *buffer;
+
+    if (!filename)
+        return (0);
+
+    buffer = malloc((letters + 1) * sizeof(char));
+    if (buffer == NULL)
+        return (0);
+
+    buffer[letters] = '\0';
+    file_desc = open(filename, O_RDONLY);
+    if (file_desc == -1)
+        return (0);
+
+    letters_read = read(file_desc, buffer, letters);
+    write(STDOUT_FILENO, buffer, letters_read);
+    close(file_desc);
+    free(buffer);
+
+    return letters_read;
+}
